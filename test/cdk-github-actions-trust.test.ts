7233ce7d763674ef0dac64b69494cc7e3905c93e
Aziz Mohamed <amohamed4@woolworths.com.au>
2025-07-04 17:07:28 +1000

    initial commit

---
+++ test/cdk-github-actions-trust.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkGithubActionsTrust from '../lib/cdk-github-actions-trust-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-github-actions-trust-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkGithubActionsTrust.CdkGithubActionsTrustStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
